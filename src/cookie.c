3b209eac673e0d0a839580a69f5acde188963d4f
Brandon L Black <blblack@gmail.com>
2020-12-26 15:47:15 +0000

    Replace a bunch of size_t with unsigned
    I've apparently gotten into the wrong mindset about size_t vs
    unsigned.  It's easy to let size_t proliferate throughout your
    code base once you start matching types with all of the standard
    library arguments and thinking of this as the "natural" type for
    anything that counts or sizes things.  It seems to have a certain
    cleanliness advantage in that it won't wrap in the real world,
    too.
    
    However, it's commonly wasteful (e.g. x86_64) when the sizes are
    known to be reasonably-limited, and better to stick with unsigned
    in the general case.
    
    We've kept size_t in places where it clearly is a good idea: when
    it's directly storing a size_t return value from a function before
    checking and converting, for our own library functions related to
    raw allocation and file-mapping, and for file data offsets in
    readers of potentially-large files (like the zonefile scanner).

--- src/cookie.c
+++ src/cookie.c
@@ -251,7 +251,7 @@ void cookie_config(const char* key_file)
 
     if (key_file) {
         if (safe_read_keyfile(key_file, master_key))
-            log_fatal("Cannot read %zu bytes from '%s': %s", (size_t)KDF_KEYBYTES, key_file, logf_errno());
+            log_fatal("Cannot read %u bytes from '%s': %s", (unsigned)KDF_KEYBYTES, key_file, logf_errno());
     } else {
         char* autokey_path = gdnsd_resolve_path_run(base_autokey, NULL);
         if (safe_read_keyfile(autokey_path, master_key)) {
@@ -284,7 +284,7 @@ void cookie_runtime_init(struct ev_loop* loop)
     ev_periodic_start(loop, hourly_p);
 }
 
-bool cookie_process(uint8_t* cookie_data_out, const uint8_t* cookie_data_in, const gdnsd_anysin_t* client, const size_t cookie_data_in_len)
+bool cookie_process(uint8_t* cookie_data_out, const uint8_t* cookie_data_in, const gdnsd_anysin_t* client, const unsigned cookie_data_in_len)
 {
     gdnsd_assume(master_key); // cookie_config() was run
 
--- src/plugins/mon.c
+++ src/plugins/mon.c
@@ -83,7 +83,7 @@ static smgr_t* smgrs = NULL;
 static gdnsd_sttl_t* smgr_sttl = NULL;
 GRCU_PUB_DEF(smgr_sttl_consumer_, NULL);
 
-static size_t max_states_len = 0;
+static unsigned max_states_len = 0;
 
 static bool initial_round = false;
 static bool testsuite_nodelay = false;
@@ -914,7 +914,7 @@ static void get_state_texts(const unsigned i, const char** cur_state_out, const 
                       [!!(smgrs[i].real_sttl & GDNSD_STTL_DOWN)];
 }
 
-char* gdnsd_mon_states_get_json(size_t* len)
+char* gdnsd_mon_states_get_json(unsigned* len)
 {
     gdnsd_assume(max_states_len);
 
@@ -933,17 +933,17 @@ char* gdnsd_mon_states_get_json(size_t* len)
         const char* cur_st;
         const char* real_st;
         get_state_texts(i, &cur_st, &real_st);
-        const size_t avail = (size_t)(max_states_len - (size_t)(buf - buf_start));
+        const unsigned avail = (unsigned)(max_states_len - (unsigned)(buf - buf_start));
         const int snp_rv = snprintf(buf, avail, json_tmpl, smgrs[i].desc, cur_st, real_st);
-        gdnsd_assume(snp_rv > 0 && (size_t)snp_rv < avail);
-        buf += (size_t)snp_rv;
+        gdnsd_assume(snp_rv > 0 && (unsigned)snp_rv < avail);
+        buf += (unsigned)snp_rv;
     }
 
     memcpy(buf, json_foot, json_foot_len);
     buf += json_foot_len;
 
     gdnsd_assume(buf > buf_start);
-    const size_t written = (size_t)(buf - buf_start);
+    const unsigned written = (unsigned)(buf - buf_start);
     gdnsd_assume(written < max_states_len);
 
     *len = written;
