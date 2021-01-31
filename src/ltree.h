520ab2140f025c78581f1c3f5766facce029ff33
Brandon L Black <blblack@gmail.com>
2021-01-31 01:27:40 +0000

    ltree: add a generation counter
    This adds a generation counter to the root-of-dns which increments
    on each successful reload of the zone data, and surfaces it in
    dnspacket's search results structure.  This is currently unused,
    but it's a prerequisite for any kind of DNSSEC-related caching so
    that caches aren't stale with relation to the zone data.

--- src/ltree.h
+++ src/ltree.h
@@ -334,7 +334,12 @@ void realize_rdata(const ltree_node_t* node, ltree_rrset_raw_t* raw);
         }\
     } while (0)
 
-// ltree_root is RCU-managed and accessed by reader threads, defined in ltree.c
-GRCU_PUB_DECL(ltree_node_t*, root_tree);
+typedef struct {
+    ltree_node_t* root;
+    uint64_t gen; // bumps on every zreload
+} ltree_root_t;
+
+// lroot is RCU-managed and accessed by reader threads, defined in ltree.c
+GRCU_PUB_DECL(ltree_root_t*, lroot);
 
 #endif // GDNSD_LTREE_H
